edeabbf057c5f11f38a18ec9fd396aba7b9c7449
SamHarron <harronsam@gmail.com>
2020-11-19 11:49:32 -0500

    commit tree 8fc65c0d52a2372889beb8734c824d2f734a372e

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+names = ["Sam Harron", "Melissa Harron", "Matthew Gibson", "Carey Gibson"]
 
+def my_collect(array)
+    x = 0
+    names_new = []
+    while x < array.length
+        names_new.push yield(array[x])
+        x += 1
+    end
+    names_new
+end
 
+my_collect(names) {|x| x.split(" ").first}
+my_collect(names) {|x| x.split(" ").last}
